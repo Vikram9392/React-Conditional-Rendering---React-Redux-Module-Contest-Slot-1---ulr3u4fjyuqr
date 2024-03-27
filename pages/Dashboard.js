d806fc6417f877bbe27320e3722355bb08151afb
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-27 16:30:59 +0530

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -3,15 +3,20 @@ import { AuthContext } from "./_app";
 
 
 function Dashboard() {
-  const { } = useContext(AuthContext);
+  const { button1, button2 } = useContext(AuthContext);
+  
 
   return (
     <div id="dashboardPage">
-      <button id="btn1" >Button 1</button>
-      <button id="btn2" >Button 2</button>
-      <div className="msg">Both are true</div>
-      <div className="msg">One on them is false</div>
-      <div className="msg">Both are false</div>
+      <button id="btn1" onClick={()=>{
+        button1.setBtn1(n=>!n)
+      }}>Button 1</button>
+      <button id="btn2" onClick={()=>{
+        button2.setBtn2(n=>!n)
+      }}>Button 2</button>
+      {button1.btn1 && button2.btn2 && <div className="msg">Both are true</div>}
+      {((!button1.btn1 && button2.btn2) ||  (button1.btn1 && !button2.btn2)) &&<div className="msg">One on them is false</div>}
+      {(!button1.btn1 && !button2.btn2) &&<div className="msg">Both are false</div>}
 
     </div>
   );
